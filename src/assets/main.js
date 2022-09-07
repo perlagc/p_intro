0026cfce3759ff2518d8396864cfcb27f44eb4aa
perlagc <58160573+perlagc@users.noreply.github.com>
2022-09-07 14:30:23 -0500

    v1
    al server

---
+++ src/assets/main.js
@@ -0,0 +1,64 @@
+/*
+cod directo de rapidapi
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4feb0bc7d1msh80bd05fe38f497bp1c2994jsnf07b7786709c',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+//aqui usa una promesa
+fetch('https://youtube-v31.p.rapidapi.com/search?channelId=UCqECaJ8Gagnn7YCbPEzWH6g&part=snippet%2Cid&order=date&maxResults=10', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+*/
+
+
+
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCqECaJ8Gagnn7YCbPEzWH6g&part=snippet%2Cid&order=date&maxResults=10';
+
+const content = null || document.getElementById('content');
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+        'X-RapidAPI-Key': 'c68edbdba9mshc3cb6eb8038f3b2p1f444ajsna5de19fe1256'
+    }
+};
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+}
+
+(async () => {
+    try {
+        const videos = await fetchData(API);
+
+        // bloque en html para 'definir' la vista de los videos, tomando info de la consulta a la api
+        let view = `
+        ${videos.items.map(video => `
+        <div class="group relative">
+            <div
+            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+            </div>
+            <div class="mt-4 flex justify-between">
+            <h3 class="text-sm text-gray-700">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                ${video.snippet.title}
+            </h3>
+            </div>
+        </div>
+        `).slice(0, 4).join('')}
+        `;
+        content.innerHTML = view;
+    } catch (error) {
+        console.log(error);
+    }
+})();
